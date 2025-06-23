25e426f88141a3ac7a925d1812a888426535ee04
dimascodes <dimasarisp53@gmail.com>
2025-06-23 13:37:23 +0700

    upload vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: localStorage.getItem('token') 
-      ? `Bearer ${localStorage.getItem('token')}` 
-      : '',
-  },
+  baseURL: "/api",
+  //  headers: {
+  //    'Content-Type': 'application/json',
+  //    Authorization: localStorage.getItem('token')
+  //      ? `Bearer ${localStorage.getItem('token')}`
+  //      : '',
+  //  },
 });
 
 // Interceptor untuk menambahkan token setelah login
- api.interceptors.request.use(config => {
-   const token = localStorage.getItem('token');
-     if (token) {
-         config.headers.Authorization = `Bearer ${token}`;
-           }
-             return config;
-             });
- 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
