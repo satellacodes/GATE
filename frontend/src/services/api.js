d105c5b53c114216233574ba65069b9bba11ca43
dimascodes <dimasarisp53@gmail.com>
2025-07-03 20:23:07 +0700

    baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const baseURL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const baseURL = "/api";
 
 const api = axios.create({
   baseURL,
