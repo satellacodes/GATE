aae888faed7cbbe3785e5b66d415bf5c79b9858f
dimascodes <dimasarisp53@gmail.com>
2025-08-28 21:25:37 +0700

    msmsms

--- frontend/api/routes/device.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const deviceController = require('../controllers/device.controller');
-
-router.post('/heartbeat', deviceController.updateDeviceStatus);
-router.get('/status', deviceController.getDeviceStatus);
-
-module.exports = router;
--- frontend/api/routes/history.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../middlewares/auth.middleware');
-const historyController = require('../controllers/history.controller');
-
-router.use(authMiddleware);
-router.get('/', historyController.getAccessHistory);
-
-module.exports = router;
